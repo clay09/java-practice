02c324a4e802f4d11fc1df062a20a8a979e00bbf
Ritta8090 <ritta8090@gmail.com>
2024-05-01 17:20:41 +0800

    資料型態

---
+++ dataTypeVariable.java
@@ -0,0 +1,12 @@
+public class dataTypeVariable{
+
+	public static void main(String args[]){
+	
+		
+		System.out.println(3); 
+		System.out.println(90000000000000L);
+		char a = '你';
+		System.out.println(a);
+	}
+	
+}
