180232a80c25cda793a9062481bee410d79c7965
clay09 <iverson110040@gmail.com>
2024-05-07 22:45:47 +0800

    123

---
+++ ConditionalStatement.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class ConditionalStatement {
+    public static void main(String args[]){
+	
+		Scanner s = new Scanner(System.in);
+        int x = s.nextInt();
+        System.out.println(x);
+		
+	}
+}
